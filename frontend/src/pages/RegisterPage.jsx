89c1973703089affde768d587dcb113785316e3e
Eric Mitchell <ericm@Eric.>
2026-04-29 17:36:03 -0400

    installed hash-wasm package, added crypto utilities, added user registration

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,8 +1,45 @@
 import { Link } from "react-router-dom";
 import PublicLayout from "../components/layout/PublicLayout";
-
+import { useState } from "react";
+import buildRegistrationPayload from "../crypto/utilities.jsx"
+import { useNavigate } from "react-router-dom";
 // Renders the /register page route.
 export default function RegisterPage() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordVerify, setPasswordVerify] = useState("")
+  const [hashedPassword, setHashedPassword] = useState("")
+
+  async function handleRegistrationSubmit(e) {
+    e.preventDefault()
+    
+    if (password !== passwordVerify) {
+      console.log("Passwords do not match")
+      return
+    }
+
+    try {
+      // const serverPayload = await buildRegistrationPayload(email, password)
+      const serverPayload = {
+        email,
+        master_password: password,
+        mfa_enabled: false
+      }
+      const response = await fetch("http://127.0.0.1:8000/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(serverPayload)
+      })
+      // const data = await response.json()
+      // console.log(response)
+      navigation('/guide')
+    } catch (err) {
+        console.error(err.message)
+  }
+
+  } 
   return (
     <PublicLayout logoPosition="center">
       <section className="auth-page">
@@ -12,7 +49,12 @@ export default function RegisterPage() {
           <form className="auth-page__form">
             <label className="auth-page__field">
               <span>Email:</span>
-              <input className="auth-page__input" type="email" name="email" />
+              <input 
+                className="auth-page__input" 
+                type="email" 
+                name="email" 
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </label>
 
             <label className="auth-page__field">
@@ -21,6 +63,7 @@ export default function RegisterPage() {
                 className="auth-page__input"
                 type="password"
                 name="password"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </label>
 
@@ -30,10 +73,12 @@ export default function RegisterPage() {
                 className="auth-page__input"
                 type="password"
                 name="confirmPassword"
+                onChange={(e) => setPasswordVerify(e.target.value)}
               />
             </label>
 
-            <button className="auth-page__button" type="submit">
+            <button className="auth-page__button" type="submit" 
+                onClick={handleRegistrationSubmit}>
               Register
             </button>
           </form>
